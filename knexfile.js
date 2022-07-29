6b02ad710645b39eca0d0350807e6f668b7830fe
Sumeet Pol <polsumeet3666@gmail.com>
2022-07-29 18:16:36 +0530

    init

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'postgres',
+      user: 'postgres',
+      password: 'example',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
